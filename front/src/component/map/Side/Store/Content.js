0c00d1962c8b16d649aa59270d8d7502aa37332a
박기찬 <rl0425@naver.com>
2023-04-20 17:38:26 +0900

    사이드 바 스토어 컨텐츠 작업

--- front/src/component/map/Side/Store/Content.js
+++ front/src/component/map/Side/Store/Content.js
@@ -1,12 +1,22 @@
 import classes from "./Content.module.css";
-import {useState} from "react";
+import {useRef, useState} from "react";
 
 function Content(props){
     const data = props.data
+
+    // 목적지 이벤트
     const [isDn, setDn] = useState(false)
+
+    // 시작, 종료 지점
     const [start, setStart] = useState("")
     const [end, setEnd] = useState("")
+
+    // 즐겨찾기 목록
     const [isFavorite, setFavorite] = useState(data.favorite ? true : false)
+
+    // 컨텐츠 active 여부
+    const [isActive, setActive] = useState(false)
+    const addContentRef = useRef()
 
     const handleDestinationOpt = (e) => {
         e.preventDefault()
@@ -56,8 +66,31 @@ function Content(props){
         }
     }
 
+    const handleIsActive = (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+
+        const temp = isActive
+
+        setActive(!temp)
+
+        if(temp) {
+            setTimeout(() => {
+                addContentRef.current.querySelectorAll('*').forEach(element => {
+                    element.style.maxHeight = '0';
+                });
+            }, 400)
+        }
+        else{
+            console.log("asdas")
+            addContentRef.current.querySelectorAll('*').forEach(element => {
+                element.style.maxHeight = '100%';
+            });
+        }
+    }
+
     return (
-        <div className={classes.content}>
+        <div className={isActive ? classes.contentActive : classes.content} onClick={(e) => handleIsActive(e)}>
             <div className={classes.mainContent}>
                 <div className={classes.left}>
                     <div className={classes.titleDiv}>
@@ -104,14 +137,79 @@ function Content(props){
                     </div>
                 </div>
             </div>
-            <div>
-                <div className={classes.menu}>
-                    <div className={classes.menuTop}><span>메뉴</span></div>
-                    <div className={classes.menuContent}>
-                        <div>
-                            <span>자연산 참가자미 모둠(1인)</span>
-                            <div></div>
-                            <label>25,000</label>
+            <div ref={addContentRef} className={isActive ? classes.addActiveContent : classes.addContent}>
+                <div className={isActive ? classes.contentActiveSet : classes.contentSet}>
+                    <div className={classes.menu}>
+                        <div className={classes.menuTop}><span>메뉴</span></div>
+                        <div className={classes.menuContents}>
+                            <div className={classes.menuContent}>
+                                <span>자연산 참가자미 모둠(1인)</span>
+                                <div className={classes.dotDiv}></div>
+                                <label>25,000</label>
+                            </div>
+                            <div className={classes.menuContent}>
+                                <span>도다리 세꼬시, 대방어 (1인)</span>
+                                <div className={classes.dotDiv}></div>
+                                <label>35,000</label>
+                            </div>
+                            <div className={classes.menuContent}>
+                                <span>줄가자미 (1인)</span>
+                                <div className={classes.dotDiv}></div>
+                                <label>48,000</label>
+                            </div>
+                            <div className={classes.menuContent}>
+                                <span>물회</span>
+                                <div className={classes.dotDiv}></div>
+                                <label>23,000</label>
+                            </div>
+                            <div className={classes.menuContent}>
+                                <span>공기밥</span>
+                                <div className={classes.dotDiv}></div>
+                                <label>2,000</label>
+                            </div>
+                            <div className={classes.menuContent}>
+                                <span>어린이 돈가스</span>
+                                <div className={classes.dotDiv}></div>
+                                <label>5,000</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={classes.talk}>
+                        <div className={classes.talkTop}><span>현장 토크</span></div>
+                        <div className={classes.talkContents}>
+                            <div className={classes.talkContent}>
+                                <img/>
+                            </div>
+                            <div className={classes.talkContent}>
+                                <span>돈까스 먹고싶어용가리 치킨도 먹고싶어용가리 치킨도 먹고싶어용가리 치킨도 먹고싶어용가리</span>
+                            </div>
+                            <div className={classes.talkContent}>
+                                <span>돈까스 먹고싶어용가리 치킨도 먹고싶어용가리 치킨도 먹고싶어용가리 치킨도 먹고싶어용가리</span>
+                            </div>
+                            <div className={classes.talkContent}>
+                                <span>돈까스 먹고싶어용가리 치킨도 먹고싶어용가리 치킨도 먹고싶어용가리 치킨도 먹고싶어용가리</span>
+                            </div>
+                            <div className={classes.talkContent}>
+                                <span>돈까스 먹고싶어용가리 치킨도 먹고싶어용가리 치킨도 먹고싶어용가리 치킨도 먹고싶어용가리</span>
+                            </div>
+
+                        </div>
+                        <div className={classes.talkWriteDiv}>
+                            <div className={classes.writeDiv}>
+                                <img src={"/images/map/writePencil.svg"}/>
+                                <span>현장토크 글쓰기</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={classes.functionSet}>
+                        <div className={classes.functionDiv}>
+                            <img src={"/images/map/share_default.svg"}/>
+                        </div>
+                        <div className={classes.functionDiv}>
+                            <img src={"/images/map/destination_default.svg"}/>
+                        </div>
+                        <div className={classes.functionDiv}>
+                            <img src={"/images/map/favorite_default.svg"}/>
                         </div>
                     </div>
                 </div>
