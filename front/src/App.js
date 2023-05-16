6685c67db68fca8b3a7d131e9f3fc5dad5069bcc
KimDeokjung <ejrwnd123@gmail.com>
2023-05-16 23:25:53 +0900

    merge conflict resolution

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,10 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-
 import Main from "./component/Main/Main"
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {CookiesProvider, useCookies} from 'react-cookie';
 import Map from "./component/map/Map";
 import Login from "./component/Login/Login"
 import SignUp from "./component/SignUp/SignUP";
 import Community from "./component/Community/Community";
-
-import { CookiesProvider } from 'react-cookie';
 
 function App() {
   return (
